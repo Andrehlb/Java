7059bbfee278faf40d526c78e5e57614a7110caf
Andrehlb <andreh.72@hotmail.com>
2025-06-18 20:50:52 -0300

    Refactor and remove outdated Java files; add new implementations for salary calculation, odd/even conditionals, multiplication table, and array sorting. Update project structure with README files and VSCode settings.

---
+++ ordenarArrayNumInt.java
@@ -0,0 +1,10 @@
+import java.util.Arrays;
+public class ordenarArrayNumInt {
+    public static void main(String[] args) {
+        int[] nums = {5, 2, 9, 1};
+        Arrays.sort(nums);
+
+        System.out.println(Arrays.toString(nums));
+    }
+}
+
